1264fff0dae3b036303a84a08aeececf8b4a365f
Jovan Gerodetti <jovan.gerodetti@titannano.de>
2017-03-12 15:54:27 +0100

    #6 - [Server] add CORS permissions

--- server/main.js
+++ server/main.js
@@ -6,6 +6,14 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    next();
+});
+
 require('./routes/goals').init(app);
 require('./routes/steps').init(app);
 
